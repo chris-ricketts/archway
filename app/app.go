0b45cb6a57537adef792b654373c73d518058aa3
Parth Desai <desaiparth08@gmail.com>
2022-05-12 09:02:11 +0400

    adding inflation reward accumulator

--- app/app.go
+++ app/app.go
@@ -203,15 +203,16 @@ var (
 
 	// module account permissions
 	maccPerms = map[string][]string{
-		gastracker.GasRewardCollector:  nil,
-		authtypes.FeeCollectorName:     nil,
-		distrtypes.ModuleName:          nil,
-		minttypes.ModuleName:           {authtypes.Minter},
-		stakingtypes.BondedPoolName:    {authtypes.Burner, authtypes.Staking},
-		stakingtypes.NotBondedPoolName: {authtypes.Burner, authtypes.Staking},
-		govtypes.ModuleName:            {authtypes.Burner},
-		ibctransfertypes.ModuleName:    {authtypes.Minter, authtypes.Burner},
-		wasm.ModuleName:                {authtypes.Burner},
+		gastracker.GasRewardCollector:         nil,
+		gastracker.InflationRewardAccumulator: nil,
+		authtypes.FeeCollectorName:            nil,
+		distrtypes.ModuleName:                 nil,
+		minttypes.ModuleName:                  {authtypes.Minter},
+		stakingtypes.BondedPoolName:           {authtypes.Burner, authtypes.Staking},
+		stakingtypes.NotBondedPoolName:        {authtypes.Burner, authtypes.Staking},
+		govtypes.ModuleName:                   {authtypes.Burner},
+		ibctransfertypes.ModuleName:           {authtypes.Minter, authtypes.Burner},
+		wasm.ModuleName:                       {authtypes.Burner},
 	}
 )
 
